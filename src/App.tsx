2d33d2587346764d5c9479506e0f055a3238d47a
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2024-07-19 13:00:32 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../lib/components";
+
+function App() {
+  return (
+    <>
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // library entry and output settings
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "fgmc-test",
+      fileName: "fgmc-test",
+    },
+    // bundler options
+    // externalize react-related imports
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+    },
+  },
+});
